5b1ac1162d9d92add14657cdff4317ef0c0aecce
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-08-31 14:19:40 +0400

    add blogs list view

--- blog_site/blogs/views.py
+++ blog_site/blogs/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from mock_data import blogs
+
+
+def blogs_view(request):
+    html = ""
+    for blog in blogs.values():
+        html += f"""
+        <h3>{blog['title']}</h3>
+        <p>By {blog['author']} on {blog["date_published"]}</p>
+        <p>{blog['content']}</p>
+        <p>Tags: {', '.join(blog['tags'])}</p>
+        """
+    return HttpResponse(html, status=200)
